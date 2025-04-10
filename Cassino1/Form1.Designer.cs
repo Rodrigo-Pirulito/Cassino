be9b3c2325dce797265f46c2fbdec2b0c5a3a19c
Rodrigo Ribeiro Rodrigues <ra2457038>
2025-04-10 08:29:38 -0300

    first commit

---
+++ Cassino1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Cassino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.btnGirar = new System.Windows.Forms.Button();
+            this.tmrGiro = new System.Windows.Forms.Timer(this.components);
+            this.lbxUltimos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.Color.Black;
+            this.lbl1.Location = new System.Drawing.Point(51, 51);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(36, 39);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "0";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl3
+            // 
+            this.lbl3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.ForeColor = System.Drawing.Color.Black;
+            this.lbl3.Location = new System.Drawing.Point(135, 51);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(36, 39);
+            this.lbl3.TabIndex = 1;
+            this.lbl3.Text = "0";
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl2
+            // 
+            this.lbl2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.ForeColor = System.Drawing.Color.Black;
+            this.lbl2.Location = new System.Drawing.Point(93, 51);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(36, 39);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "0";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGirar
+            // 
+            this.btnGirar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGirar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnGirar.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGirar.Location = new System.Drawing.Point(19, 142);
+            this.btnGirar.Name = "btnGirar";
+            this.btnGirar.Size = new System.Drawing.Size(197, 51);
+            this.btnGirar.TabIndex = 3;
+            this.btnGirar.Text = "Girar";
+            this.btnGirar.UseVisualStyleBackColor = true;
+            this.btnGirar.Click += new System.EventHandler(this.btnGirar_Click);
+            // 
+            // tmrGiro
+            // 
+            this.tmrGiro.Tick += new System.EventHandler(this.tmrGiro_Tick);
+            // 
+            // lbxUltimos
+            // 
+            this.lbxUltimos.FormattingEnabled = true;
+            this.lbxUltimos.Location = new System.Drawing.Point(19, 242);
+            this.lbxUltimos.Name = "lbxUltimos";
+            this.lbxUltimos.Size = new System.Drawing.Size(197, 147);
+            this.lbxUltimos.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(229, 469);
+            this.Controls.Add(this.lbxUltimos);
+            this.Controls.Add(this.btnGirar);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cassino";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btnGirar;
+        private System.Windows.Forms.Timer tmrGiro;
+        private System.Windows.Forms.ListBox lbxUltimos;
+    }
+}
---
+++ Cassino1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cassino
+{
+    public partial class Form1 : Form
+    {
+        int[] roleta;
+        int[] ciclos;
+        Label[] tela;
+        Random r;
+        public Form1()
+        {
+            InitializeComponent();
+            roleta = new int[3];
+            ciclos = new int[3];
+            tela = new Label[] { lbl1, lbl2, lbl3 };
+            r = new Random();
+            for (int i = 0; i < roleta.Length; i++)
+            {
+                roleta[i] = r.Next(0, 10);
+                Atualizar(i);
+            }
+        }
+
+        void Atualizar(int i)
+        {
+            tela[i].Text = roleta[i].ToString();
+        }
+
+        private void btnGirar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ciclos.Length; i++)
+            {
+                ciclos[i] = r.Next(1, 21);
+                tela[i].ForeColor = Color.Black;
+            }
+            Array.Sort(ciclos);
+            btnGirar.Enabled = false;
+            tmrGiro.Enabled = true;
+        }
+
+        void verificarVitoria() 
+        {
+            if (lbl1.Text == lbl2.Text && lbl2.Text == lbl3.Text)
+            {
+                lbl1.ForeColor = lbl2.ForeColor = lbl3.ForeColor = Color.Green;
+                MessageBox.Show("VOCÊ VENCEU!");
+            }
+        }
+
+        private void tmrGiro_Tick(object sender, EventArgs e)
+        {
+            bool parado = true;
+            for (int i = 0; i < ciclos.Length; i++)
+            {
+                if (ciclos[i] > 0)
+                {
+                    ciclos[i]--;
+                    if (ciclos[i] == 0)
+                        tela[i].ForeColor = Color.Red;
+                    roleta[i]++;
+                    if (roleta[i] == 10)
+                        roleta[i] = 0;
+                    Atualizar(i);
+                    parado &= false;
+                }
+                else
+                {
+                    parado &= true;
+                }
+            }
+            if (parado)
+            {
+                tmrGiro.Enabled = false;
+                btnGirar.Enabled = true;
+                verificarVitoria();
+
+                lbxUltimos.Items.Add($"{roleta[0]}-{roleta[1]}-{roleta[2]}\n");
+            }
+        }
+    }
+}
