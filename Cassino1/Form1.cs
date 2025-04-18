879dc6817f1bb77e7f2a7d6b5d285208e223d8c3
Rodrigo-Pitbull-Do-Fank <rr.rodrigues@unesp.br>
2025-04-17 22:05:13 -0300

    Update Form1.cs

--- Cassino1/Form1.cs
+++ Cassino1/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,82 +8,122 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Cassino
+namespace CassinoSimulator
 {
     public partial class Form1 : Form
     {
-        int[] roleta;
-        int[] ciclos;
-        Label[] tela;
+        int[] roulette;
+        int[] cycles;
+        Label[] screen;
         Random r;
         public Form1()
         {
             InitializeComponent();
-            roleta = new int[3];
-            ciclos = new int[3];
-            tela = new Label[] { lbl1, lbl2, lbl3 };
+            roulette = new int[3];
+            cycles = new int[3];
+            screen = new Label[] { lblN1, lblN2, lblN3 };
             r = new Random();
-            for (int i = 0; i < roleta.Length; i++)
+            for (int i = 0; i < roulette.Length; i++)
             {
-                roleta[i] = r.Next(0, 10);
+                roulette[i] = r.Next(0, 10);
                 Atualizar(i);
             }
         }
-
         void Atualizar(int i)
         {
-            tela[i].Text = roleta[i].ToString();
+            screen[i].Text = roulette[i].ToString();
         }
 
-        private void btnGirar_Click(object sender, EventArgs e)
+        private void BtnSpin_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < ciclos.Length; i++)
+            for (int i = 0; i < cycles.Length; i++)
             {
-                ciclos[i] = r.Next(1, 21);
-                tela[i].ForeColor = Color.Black;
+                cycles[i] = r.Next(1, 21);
+                screen[i].ForeColor = Color.Black;
             }
-            Array.Sort(ciclos);
-            btnGirar.Enabled = false;
-            tmrGiro.Enabled = true;
+            Array.Sort(cycles);
+            BtnSpin.Enabled = false;
+            tmrSpin.Enabled = true;
         }
-
-        void verificarVitoria() 
+        void verifyWin()
         {
-            if (lbl1.Text == lbl2.Text && lbl2.Text == lbl3.Text)
+            if (lblN1.Text == lblN2.Text && lblN2.Text == lblN3.Text)
             {
-                lbl1.ForeColor = lbl2.ForeColor = lbl3.ForeColor = Color.Green;
-                MessageBox.Show("VOCÊ VENCEU!");
+                lblN1.ForeColor = Color.Green;
+                lblN2.ForeColor = Color.Green;
+                lblN3.ForeColor = Color.Green;
+                MessageBox.Show("VOCE GANHOU!!!!");
             }
-        }
-
-        private void tmrGiro_Tick(object sender, EventArgs e)
-        {
-            bool parado = true;
-            for (int i = 0; i < ciclos.Length; i++)
+            else
             {
-                if (ciclos[i] > 0)
+                if (chbVitorias.Checked)
                 {
-                    ciclos[i]--;
-                    if (ciclos[i] == 0)
-                        tela[i].ForeColor = Color.Red;
-                    roleta[i]++;
-                    if (roleta[i] == 10)
-                        roleta[i] = 0;
-                    Atualizar(i);
-                    parado &= false;
+                    jogadas.Add($"{roulette[0]}-{roulette[1]}-{roulette[2]}");
                 }
                 else
                 {
-                    parado &= true;
+                    lbxUltimos.Items.Add($"{roulette[0]}-{roulette[1]}-{roulette[2]}");
                 }
             }
-            if (parado)
-            {
-                tmrGiro.Enabled = false;
-                btnGirar.Enabled = true;
-                verificarVitoria();
+        }
 
-                lbxUltimos.Items.Add($"{roleta[0]}-{roleta[1]}-{roleta[2]}\n");
+        private void tmrSpin_Tick(object sender, EventArgs e)
+        {
+            bool stopped = true;
+            for (int i = 0; i < cycles.Length; i++)
+            {
+                if (cycles[i] > 0)
+                {
+                    cycles[i]--;
+                    if (cycles[i] == 0)
+                        screen[i].ForeColor = Color.Red;
+                    roulette[i]++;
+                    if (roulette[i] == 10)
+                        roulette[i] = 0;
+                    Atualizar(i);
+                    stopped &= false;
+                }
+                else
+                {
+                    stopped &= true;
+                }
+            }
+            if (stopped)
+            {
+                tmrSpin.Enabled = false;
+                BtnSpin.Enabled = true;
+                verifyWin();
+            }
+
+        }
+
+        List<string> jogadas;
+        private void chbVitorias_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chbVitorias.Checked)
+            {
+                jogadas = new List<string>();
+                foreach (string item in lbxUltimos.Items)
+                {
+                    jogadas.Add(item);
+                }
+
+                lbxUltimos.Items.Clear();
+                foreach (string item in jogadas)
+                {
+                    string[] nums = item.Split('-');
+                    if (nums[0] == nums[1] && nums[1] == nums[2])
+                        lbxUltimos.Items.Add(item);
+                }
+            }
+            else
+            {
+                lbxUltimos.Items.Clear();
+                foreach (string item in jogadas)
+                {
+                    lbxUltimos.Items.Add(item);
+                }
+
             }
         }
     }
